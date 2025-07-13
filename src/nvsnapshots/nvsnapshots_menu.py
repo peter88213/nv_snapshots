9e773c76a39fa141f70ba705610b1ae3f97481dd
peter88213 <petert88213@gmail.com>
2025-07-13 21:01:14 +0200

    Fixed docstring.

--- src/nvsnapshots/nvsnapshots_menu.py
+++ src/nvsnapshots/nvsnapshots_menu.py
@@ -1,7 +1,7 @@
 """Provide a menu class for the snapshots manager.
 
 Copyright (c) 2025 Peter Triesberger
-For further information see https://github.com/peter88213/nvsnapshots
+For further information see https://github.com/peter88213/nv_snapshots
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from nvsnapshots.nvsnapshots_globals import icons
