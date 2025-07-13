43a79f34373e114908d74108cc023f5c8ce6bac8
peter88213 <petert88213@gmail.com>
2025-07-13 22:57:39 +0200

    Removed dead code.

--- src/nvsnapshots/platform/platform_settings.py
+++ src/nvsnapshots/platform/platform_settings.py
@@ -7,24 +7,19 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 import platform
 
 from nvsnapshots.platform.generic_keys import GenericKeys
-from nvsnapshots.platform.generic_mouse import GenericMouse
 from nvsnapshots.platform.mac_keys import MacKeys
 from nvsnapshots.platform.windows_keys import WindowsKeys
 
 if platform.system() == 'Windows':
     PLATFORM = 'win'
     KEYS = WindowsKeys()
-    MOUSE = GenericMouse
 elif platform.system() in ('Linux', 'FreeBSD'):
     PLATFORM = 'ix'
     KEYS = GenericKeys()
-    MOUSE = GenericMouse
 elif platform.system() == 'Darwin':
     PLATFORM = 'mac'
     KEYS = MacKeys()
-    MOUSE = GenericMouse
 else:
     PLATFORM = ''
     KEYS = GenericKeys()
-    MOUSE = GenericMouse
 
